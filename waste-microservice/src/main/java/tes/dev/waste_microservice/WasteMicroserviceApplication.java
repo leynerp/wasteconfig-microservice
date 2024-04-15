6a6adcc87c40b572880edd37db951b36a11026ac
shonder <shonder@gmail.com>
2024-04-15 10:11:24 -0800

    init commit  add a project which modules, for each microservice  add a folder struct form waste-microservice

---
+++ waste-microservice/src/main/java/tes/dev/waste_microservice/WasteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package tes.dev.waste_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteMicroserviceApplication.class, args);
+	}
+
+}
---
+++ waste-microservice/src/test/java/tes/dev/waste_microservice/WasteMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tes.dev.waste_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
